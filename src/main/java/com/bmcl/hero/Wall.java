0f11e8f7842ed70ec1f62e936bccb1371effd46f
Goncalo <a043540@umaia.pt>
2023-12-01 18:01:08 +0000

    Arena terminada

--- src/main/java/com/bmcl/hero/Wall.java
+++ src/main/java/com/bmcl/hero/Wall.java
@@ -1,6 +1,7 @@
 package com.bmcl.hero;
 
 import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
@@ -18,7 +19,25 @@ public class Wall extends Element {
     }
 
     public void draw(TextGraphics graphics) {
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#FF5722"));
-        graphics.putString(new TerminalPosition(position.getX(), position.getY()), " ");
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#0000FF"));
+        // graphics.putString(new TerminalPosition(position.getX(), position.getY()), " ");
+
+
+
+        graphics.fillRectangle(new TerminalPosition(0, 5), new TerminalSize(35, 1), ' ');
+        graphics.fillRectangle(new TerminalPosition(45, 5), new TerminalSize(35, 1), ' ');
+
+        graphics.fillRectangle(new TerminalPosition(0, 11), new TerminalSize(7, 1), ' ');
+        graphics.fillRectangle(new TerminalPosition(24, 10), new TerminalSize(35, 1), ' ');
+        graphics.fillRectangle(new TerminalPosition(73, 11), new TerminalSize(7, 1), ' ');
+
+
+
+        graphics.fillRectangle(new TerminalPosition(0, 15), new TerminalSize(30, 1), ' ');
+        graphics.fillRectangle(new TerminalPosition(50, 15), new TerminalSize(30, 1), ' ');
+
+
+        graphics.fillRectangle(new TerminalPosition(0, 20), new TerminalSize(80, 1), ' ');
+
     }
 }
