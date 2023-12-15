5162cd7a6fbf3cb1ad64735e5499060a8e6ecdee
Otávio A <otavio200100@hotmail.com>
2023-12-15 22:05:55 +0000

    Hero move tests done

--- src/test/java/com/bmcl/hero/ArenaTest.java
+++ src/test/java/com/bmcl/hero/ArenaTest.java
@@ -7,5 +7,24 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArenaTest {
+    Hero hero = new Hero(34,19);
+    Game game;
 
+    {
+        try {
+            game = new Game(80,21);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Arena arena = new Arena(80,21,game);
+
+    @Test
+    public void moveupTest() throws IOException {
+        Position expected = new Position(34,18);
+        hero.setPosition(hero.moveUp());
+        System.out.println(hero.position.getY());
+        assertEquals(hero.position.getY(), expected.getY());
+    }
 }
--- src/test/java/com/bmcl/hero/HeroTest.java
+++ src/test/java/com/bmcl/hero/HeroTest.java
@@ -1,11 +1,36 @@
 package com.bmcl.hero;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
-    @BeforeEach
+    Hero hero = new Hero(1,1);
+    @Test
+    public void moveUpTest(){
+        Position expected = new Position(1,0);
+        hero.setPosition(hero.moveUp());
+        assertEquals(hero.getPosition(), expected);
+    }
 
-    @test
+    @Test
+    public void moveDownTest(){
+        Position expected = new Position(1,2);
+        hero.setPosition(hero.moveDown());
+        assertEquals(hero.getPosition(), expected);
+    }
+    @Test
+    public void moveLeftTest(){
+        Position expected = new Position(0,1);
+        hero.setPosition(hero.moveLeft());
+        assertEquals(hero.getPosition(), expected);
+    }
+    @Test
+    public void moveRightTest(){
+        Position expected = new Position(2,1);
+        hero.setPosition(hero.moveRight());
+        assertEquals(hero.getPosition(), expected);
+    }
+
 }
