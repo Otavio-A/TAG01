485de013de2f4c20900216b406677c9e2337e408
Diogo <110359163+Goncalves745@users.noreply.github.com>
2024-01-04 17:37:52 +0000

    comentado maior parte do codigo

--- src/main/java/com/bmcl/hero/Menu.java
+++ src/main/java/com/bmcl/hero/Menu.java
@@ -36,7 +36,10 @@ public class Menu {
 
 
         for (int i = 0; i < 2; i++) {
+            //Define a cor do texto dependendo da opção que estiver selecionada
             graphics.setForegroundColor(i == selectedOptionIndex ? TextColor.ANSI.GREEN : TextColor.ANSI.RED);
+
+            //Desenha o texto da opcao start ou exit na posicao especifica
             graphics.putString(new TerminalPosition(38, 5 + i * 2), i == selectedOptionIndex ? "> " : "  ");
             graphics.putString(new TerminalPosition(40, 5 + i * 2), i == 0 ? "START" : "EXIT");
         }
@@ -44,10 +47,15 @@ public class Menu {
 
 
     public void processKey(KeyStroke key) {
+
+        //Verifica se não é null
         if (key != null) {
+            //Verifica se o tipo de tecla e a arrowup
             if (key.getKeyType() == KeyType.ArrowUp) {
+                //Se for a arrowup anda para a seleção em cima
                 selectedOptionIndex = (selectedOptionIndex - 1 + 2) % 2;
             } else if (key.getKeyType() == KeyType.ArrowDown) {
+                //Se for a arrowdown anda para a seleção em baixo
                 selectedOptionIndex = (selectedOptionIndex + 1) % 2;
             }
         }
